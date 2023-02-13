e13806fa6b229c411b5174f6313abd2d2c3b807c
seo-juhyeok <jujuh0313@gmail.com>
2023-02-13 17:02:16 +0900

    Update board.dart

--- hexa_delivery/lib/board.dart
+++ hexa_delivery/lib/board.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class BoardPage extends StatefulWidget {
@@ -8,27 +10,22 @@ class BoardPage extends StatefulWidget {
 }
 
 String food = '음식 종류';
-String store1 = '가게1';
-String people1 = '1';
-String time1 = 'a1';
-String cost1 = '1';
-String store2 = '가게명2';
-String people2 = '2';
-String time2 = 'a2';
-String cost2 = '2';
-String store3 = '가게명3';
-String people3 = '3';
-String time3 = 'a3';
-String cost3 = '3';
-String store4 = '가게명4';
-String people4 = '4';
-String time4 = 'a4';
-String cost4 = '4';
-String store5 = '가게명5';
-String people5 = '5';
-String time5 = 'a5';
-String cost5 = '5';
+class Store {
+  String name;
+  int number;
+  String time;
+  int cost;
 
+  Store(this.name, this.number, this.time, this.cost);
+}
+
+void main() {
+  Store p1 = Store('test', 10, 'test', 10);
+  String Storename = p1.name;
+  int Storenumber = p1.number;
+  String Storetime = p1.time;
+  int Storecost = p1.cost;
+}
 
 class _BoardPageState extends State<BoardPage> {
   @override
@@ -57,244 +54,80 @@ class _BoardPageState extends State<BoardPage> {
                     fontSize: 20.0)
                   ),
               ),
-              GestureDetector(
-                onTap: () {
-                  // print('First button clicked');
-                  Navigator.push(context, MaterialPageRoute(
-                    builder: (context) => TestPage()));
-                },
-                child: Container(
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        top: 7,
-                        left: 15,
-                        child: Text(store1,
-                          style: TextStyle(
-                            fontSize: 17.0,
-                            fontWeight: FontWeight.w600
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        top: 10,
-                        right: 15,
-                        child: Text('현재 '+people1+'명')
-                      ),
-                      Positioned(
-                        bottom: 10,
-                        left: 15,
-                        child: Text('주문시간 '+time1+'시'),
-                      ),
-                      Positioned(
-                        bottom: 10,
-                        right: 50,
-                        child: Text('배달료 '+cost1+'원'),
-                      ),
-                    ],
-                  ),
-                  margin: EdgeInsets.fromLTRB(30, 0, 30, 0),
-                  width: 350,
-                  height: 75,
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 83, 216, 236),
-                    borderRadius: BorderRadius.circular(20),
-                    border: Border.all(color: Colors.black, width: 2)
-                  ),
-                ),
-              ),
+              ContainerBuilder(context),
               SizedBox(
                 height: 15,
                 width: 350,
               ),
-              GestureDetector(
-                onTap: () {
-                  print('Second button clicked');
-                },
-                child: Container(
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        top: 7,
-                        left: 15,
-                        child: Text('가게명',
-                          style: TextStyle(
-                            fontSize: 17.0,
-                            fontWeight: FontWeight.w600
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        top: 10,
-                        right: 15,
-                        child: Text('현재 a명')
-                      ),
-                      Positioned(
-                        bottom: 10,
-                        left: 15,
-                        child: Text('주문시간 a시'),
-                      ),
-                      Positioned(
-                        bottom: 10,
-                        right: 50,
-                        child: Text('배달료 a원'),
-                      ),
-                    ],
-                  ),
-                  width: 350,
-                  height: 75,
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 83, 216, 236),
-                    borderRadius: BorderRadius.circular(20),
-                    border: Border.all(color: Colors.black, width: 2)
-                  ),
-                ),
-              ),
+              ContainerBuilder(context),
               SizedBox(
                 height: 15,
                 width: 350,
               ),
-              GestureDetector(
-                onTap: () {
-                  print('Third button clicked');
-                },
-                child: Container(
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        top: 7,
-                        left: 15,
-                        child: Text('가게명',
-                          style: TextStyle(
-                            fontSize: 17.0,
-                            fontWeight: FontWeight.w600
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        top: 10,
-                        right: 15,
-                        child: Text('현재 a명')
-                      ),
-                      Positioned(
-                        bottom: 10,
-                        left: 15,
-                        child: Text('주문시간 a시'),
-                      ),
-                      Positioned(
-                        bottom: 10,
-                        right: 50,
-                        child: Text('배달료 a원'),
-                      ),
-                    ],
-                  ),
-                  width: 350,
-                  height: 75,
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 83, 216, 236),
-                    borderRadius: BorderRadius.circular(20),
-                    border: Border.all(color: Colors.black, width: 2)
-                  ),
-                ),
-              ),
+              ContainerBuilder(context),
               SizedBox(
                 height: 15,
                 width: 350,
               ),
-              GestureDetector(
-                onTap: () {
-                  print('Fourth button clicked');
-                },
-                child: Container(
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        top: 7,
-                        left: 15,
-                        child: Text('가게명',
-                          style: TextStyle(
-                            fontSize: 17.0,
-                            fontWeight: FontWeight.w600
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        top: 10,
-                        right: 15,
-                        child: Text('현재 a명')
-                      ),
-                      Positioned(
-                        bottom: 10,
-                        left: 15,
-                        child: Text('주문시간 a시'),
-                      ),
-                      Positioned(
-                        bottom: 10,
-                        right: 50,
-                        child: Text('배달료 a원'),
-                      ),
-                    ],
-                  ),
-                  width: 350,
-                  height: 75,
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 83, 216, 236),
-                    borderRadius: BorderRadius.circular(20),
-                    border: Border.all(color: Colors.black, width: 2)
-                  ),
-                ),
-              ),
+              ContainerBuilder(context),
               SizedBox(
                 height: 15,
                 width: 350,
               ),
-              GestureDetector(
-                onTap: () {
-                  print('Fifth button clicked');
-                },
-                child: Container(
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        top: 7,
-                        left: 15,
-                        child: Text('가게명',
-                          style: TextStyle(
-                            fontSize: 17.0,
-                            fontWeight: FontWeight.w600
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        top: 10,
-                        right: 15,
-                        child: Text('현재 a명')
-                      ),
-                      Positioned(
-                        bottom: 10,
-                        left: 15,
-                        child: Text('주문시간 a시'),
-                      ),
-                      Positioned(
-                        bottom: 10,
-                        right: 50,
-                        child: Text('배달료 a원'),
-                      ),
-                    ],
-                  ),
-                  width: 350,
-                  height: 75,
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 83, 216, 236),
-                    borderRadius: BorderRadius.circular(20),
-                    border: Border.all(color: Colors.black, width: 2)
-                  ),
-                ),
-              ),
+              ContainerBuilder(context),
             ],
           ),
       ),
     );
+  }
+
+
+  GestureDetector ContainerBuilder(BuildContext context) {
+    return GestureDetector(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(
+                  builder: (context) => TestPage()));
+              },
+              child: Container(
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: 7,
+                      left: 15,
+                      child: Text('test',
+                        style: TextStyle(
+                          fontSize: 17.0,
+                          fontWeight: FontWeight.w600
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: 10,
+                      right: 15,
+                      child: Text('현재 '+'a'+'명')
+                    ),
+                    Positioned(
+                      bottom: 10,
+                      left: 15,
+                      child: Text('주문시간 '+'a'+'시'),
+                    ),
+                    Positioned(
+                      bottom: 10,
+                      right: 50,
+                      child: Text('배달료 '+'a'+'원'),
+                    ),
+                  ],
+                ),
+                margin: EdgeInsets.fromLTRB(30, 0, 30, 0),
+                width: 350,
+                height: 75,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 83, 216, 236),
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(color: Colors.black, width: 2)
+                ),
+              ),
+            );
   }
 }
 
