67e48da5f63fcf2bb3185ced5304a2f1220e0d42
준화 <junwha0511@naver.com>
2023-07-21 20:36:56 +0900

    Integrate my page with extracted card

--- hexa_delivery/lib/pages/my_order_page.dart
+++ hexa_delivery/lib/pages/my_order_page.dart
@@ -3,6 +3,7 @@ import 'package:hexa_delivery/model/category.dart';
 import 'package:hexa_delivery/model/dto.dart';
 import 'package:hexa_delivery/pages/detail_page.dart';
 import 'package:hexa_delivery/theme/theme_data.dart';
+import 'package:hexa_delivery/widgets/order_desc_card.dart';
 
 class MyOrderPage extends StatefulWidget {
   const MyOrderPage({super.key});
@@ -65,7 +66,7 @@ class _MyOrderPageState extends State<MyOrderPage> {
   Widget buildCancelContainer(BuildContext context, OrderDTO order) {
     return Row(
       children: [
-        Expanded(child: buildContainer(context, order)),
+        Expanded(child: OrderDescCard(order)),
         Container(
           decoration: const BoxDecoration(
             borderRadius: BorderRadius.all(Radius.circular(10)),
@@ -94,86 +95,6 @@ class _MyOrderPageState extends State<MyOrderPage> {
           ),
         )
       ],
-    );
-  }
-
-  GestureDetector buildContainer(BuildContext context, OrderDTO order) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(context,
-            MaterialPageRoute(builder: (context) => DetailPage(order.oid)));
-      },
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        child: Container(
-          height: 80,
-          decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 240, 240, 240),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 18.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      store.name,
-                      style: const TextStyle(
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.w800,
-                        color: Color(kThemeColorHEX),
-                      ),
-                    ),
-                    const SizedBox(height: 3),
-                    Row(
-                      children: [
-                        const Text(
-                          '주문시간 ',
-                          style: TextStyle(
-                              color: Color.fromARGB(255, 99, 118, 119),
-                              fontSize: 14.0,
-                              fontWeight: FontWeight.w600),
-                        ),
-                        Text(
-                          '${order.expTime.hour}시 ${order.expTime.minute}분',
-                          style: const TextStyle(
-                              fontSize: 14.0, fontWeight: FontWeight.w600),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              // Padding(
-              //   padding: const EdgeInsets.only(right: 18),
-              //   child: Column(
-              //     mainAxisAlignment: MainAxisAlignment.center,
-              //     crossAxisAlignment: CrossAxisAlignment.end,
-              //     children: [
-              //       Text(
-              //         '현재 ${order.numOfMembers}명 참가중',
-              //         style: const TextStyle(
-              //           fontSize: 14.0,
-              //           fontWeight: FontWeight.w600,
-              //         ),
-              //       ),
-              //       const SizedBox(height: 3),
-              //       Text(
-              //         '배달료 ${order.fee}원',
-              //         style: const TextStyle(
-              //             fontSize: 14.0, fontWeight: FontWeight.w400),
-              //       ),
-              //     ],
-              //   ),
-              // ),
-            ],
-          ),
-        ),
-      ),
     );
   }
 }
